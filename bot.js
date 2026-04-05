364380efc9863d7d29f04044b77d5c7161f84ffe
karunyapandey <karunyapandey2006@gmail.com>
2026-04-05 21:07:13 +0530

    puppeteer

--- bot.js
+++ bot.js
@@ -16,9 +16,15 @@ const client = new Client({
   authStrategy: new LocalAuth(),
   puppeteer: {
     headless: true,
-    args: ["--no-sandbox", "--disable-setuid-sandbox"]
+    executablePath: "/usr/bin/chromium",
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--disable-dev-shm-usage",
+      "--disable-gpu"
+    ]
   }
-})
+});
 
 // Show QR code first time
 client.on("qr", (qr) => {
